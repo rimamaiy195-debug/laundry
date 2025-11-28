f6465e0d196da6fffb60acb8bb4a29d8eed2667a
rimamaiy195-debug <rimamaiy195@gmail.com>
2025-11-28 09:08:52 +0700

    Initial commit

---
+++ admin/footer.php
@@ -0,0 +1,36 @@
+<footer class="bg-dark text-white py-5" style="background-image: url('images/footer-bg.jpg'); background-size: cover;">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h5><i class="fas fa-info-circle"></i> Tentang LaundryKu</h5>
+        <p>Kami adalah penyedia layanan laundry profesional dengan pengalaman bertahun-tahun. Kami mengutamakan kualitas, kecepatan, dan kepuasan pelanggan.</p>
+      </div>
+      <div class="col-md-3">
+        <h5><i class="fas fa-link"></i> Tautan Cepat</h5>
+        <ul class="list-unstyled">
+          <li><a href="#" class="text-white">Beranda</a></li>
+          <li><a href="#" class="text-white">Layanan</a></li>
+          <li><a href="#" class="text-white">Harga</a></li>
+          <li><a href="#" class="text-white">Kontak</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h5><i class="fas fa-envelope"></i> Langganan Newsletter</h5>
+        <p>Dapatkan informasi terbaru tentang promo dan penawaran menarik dari kami.</p>
+        <form>
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" placeholder="Email Anda">
+            <div class="input-group-append">
+              <br><br>
+              <button class="btn btn-primary" type="submit">Langganan</button>
+              <br><br>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="text-center py-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    &copy; 2025 LaundryKu. All rights reserved.
+  </div>
+</footer>
---
+++ admin/pelanggan_aksi.php
@@ -0,0 +1,12 @@
+<?php
+
+include '../koneksi.php';
+
+$nama = $_POST['nama'];
+$hp = $_POST['hp'];
+$alamat = $_POST['alamat'];
+
+mysqli_query($koneksi, "insert into pelanggan values('', '$nama', '$hp', '$alamat')");
+
+echo "<script>alert('Data Tersimpan'); window.location.href='pelanggan.php'</script>";
+?>
